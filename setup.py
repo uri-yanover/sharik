db6b77c3fad96f398ce10cdfb222e4918cb5cb35
uri-yanover <uri.yanover+github@gmail.com>
2019-10-21 11:40:12 +0300

    Travis first iteration

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sharik",
+    version="0.1",
+    packages=find_packages(),
+    description='A shar(1)-like utility with a programmatic fine-tuned API',
+    author='Uri Yanover',
+    author_email='throwaway+sharik@yanover.name',
+    url='https://github.com/uri-yanover/sharik',
+    entry_points={
+        'console_scripts': [
+            'sharik = sharik.cli:main'
+        ]
+    },
+    install_requires=['click']
+)
--- sharik/cli.py
+++ sharik/cli.py
@@ -1,2 +1,4 @@
 #!/usr/bin/env python
 
+def main():
+    pass
