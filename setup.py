6dcbceda1ea26d12a275da3fce82b2509faa4209
uri-yanover <uri.yanover+github@gmail.com>
2020-09-08 11:14:17 +0300

    Update setup.py
    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sharik",
-    version="0.2.2",
+    version="0.3.0",
     packages=find_packages(),
     description='A shar(1)-like utility with a programmatic fine-tuned API',
     author='Uri Yanover',
